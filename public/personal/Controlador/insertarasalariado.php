655dea7cd42b1980d67419ec69891bef0aaa791d
sergiocg <sergioc_g@hotmail.es>
2024-06-14 08:21:13 +0200

    Modificaciones de estilo

--- public/personal/Controlador/insertarasalariado.php
+++ public/personal/Controlador/insertarasalariado.php
@@ -5,19 +5,7 @@
     $nombre = $_POST['nombre'];
     $apellidos = $_POST['apellidos'];
     $Fecha_Alta = $_POST['fecha_alta'];
-    $Id_Puesto = $_POST['puesto'];
-    
-    /*Validacion DIN
-    $partes = explode('-', $dni);
-    $numeros = $partes[0];
-    $letra = strtoupper($partes[1]);
-
-    if (substr("TRWAGMYFPDXBNJZSQVHLCKE",$numeros%23,1) == $letra)
-       echo '<p>El DNI: '.$nif.' es correcto!</p>';
-    else
-       echo '<p>La letra introducida no es corrrecta!</p>';
-    
-*/  
-
+    $Id_Puesto = $_POST['puesto'];    
+   
     $resultado = $insertData->InsertarAsalariado($dni, $nombre, $apellidos, $Id_Puesto, $Fecha_Alta);    
 ?>
