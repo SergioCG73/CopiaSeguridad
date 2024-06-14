3922126ea47c8f8ec4bcb3e38725290288d474e7
sergiocg <sergioc_g@hotmail.es>
2024-06-14 08:22:11 +0200

    Se modifica el enlace donde regresa tras añadir el nuevo registro

--- public/personal/Modelo/Asalariados.php
+++ public/personal/Modelo/Asalariados.php
@@ -55,7 +55,7 @@
 
                 echo "<script type='text/javascript'>
                         alert('Registro guardado correctamente');
-                        window.location.href='../Vista/indexPersonal.php';
+                        window.location.href='../indexPersonal.php';
                     </script>";                
             } catch(PDOException $e) {                
                 throw new Exception("Error de consulta: " .$e->getMessage());
