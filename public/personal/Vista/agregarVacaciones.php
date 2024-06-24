07583e434e2621254585a3bfc54d971c549c0413
sergiocg <sergioc_g@hotmail.es>
2024-06-24 14:16:49 +0200

    Se anade filling_tipos.php para rellenar las opciones del select

--- public/personal/Vista/agregarVacaciones.php
+++ public/personal/Vista/agregarVacaciones.php
@@ -22,7 +22,9 @@
     <br><br>
     <label>Tipo </label>
     <select name="tipo" id="tipo">
-        <option value="1">Vacaciones</option>
+    <?php
+                include_once("../Controlador/filling_tipos.php");  
+      /*  <option value="1">Vacaciones</option>
         <option value="2">Enfermedad común</option>
         <option value="3">Baja laboral</option>
         <option value="4">Permiso maternidad/paternidad</option>
@@ -30,7 +32,8 @@
         <option value="6">Permiso por matrimonio</option>
         <option value="7">Permiso NO retribuido</option>
         <option value="8">Permiso por traslado vivienda</option>
-        <option value="10">Horas sindicales</option>
+        <option value="10">Horas sindicales</option>*/
+    ?>
     </select>
     <br><br>
     <label>Notas</label>
@@ -38,27 +41,7 @@
     <br>
     <input type="submit" value="Agregar">
 
-    <!--<label>Fechas</label>
-    <input type="date" name="fecha_inicio" required>
-    <input type="date" name="fecha_final">    
-    <br><br>
-    <label>Tipo </label>
-    <select name="tipo" id="tipo">
-        <option value="1">Vacaciones</option>
-        <option value="2">Enfermedad común</option>
-        <option value="3">Baja laboral</option>
-        <option value="4">Permiso maternidad/paternidad</option>
-        <option value="5">Permiso nacimiento/fallecimiento/enfermedad grave familiar</option>
-        <option value="6">Permiso por matrimonio</option>
-        <option value="7">Permiso NO retribuido</option>
-        <option value="8">Permiso por traslado vivienda</option>
-        <option value="10">Horas sindicales</option>
-    </select>
-    <br><br>
-    <label>Notas</label>
-    <input type="textarea" name="notas">
-    <br><br>
-    <input type="submit" value="SUBMIT">-->
+   
 </form>
 <div class="item">
     <a class="boton" href="../indexPersonal.php">INICIO</a>            
