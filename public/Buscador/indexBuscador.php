4d98fa50aa5d59c3b2ab1c6ab336d49c45128676
sergiocg <sergioc_g@hotmail.es>
2024-06-26 18:45:39 +0200

    Varias modificaciones

--- public/Buscador/indexBuscador.php
+++ public/Buscador/indexBuscador.php
@@ -8,35 +8,11 @@
     <title>Buscador</title>
 </head>
 <body>
-<div class="container">
-    <!--<div class="fila">-->
-        <header>BUSCADOR DE PRODUCCIONES POR FECHAS </header>
-    <!--</div>-->
-    <fieldset><legend>Datos de búsqueda </legend>
+<div class="container">    
+        <header>BUSCADOR DE PRODUCCIONES POR FECHAS </header>    
+    <fieldset><legend>Datos de la búsqueda </legend>
         <form name="Buscador" method="post" action="encabezadostabla.php" target="_blank" id="formulario">                        
-            <table>
-                <thead>
-                    <th>Fecha Inicial</th>
-                    <th>Fecha Final</th>
-                    <th>Producto</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><input type="date" name="desde" min="2022-01-01" id="desde" required></td>
-                        <td><input type="date" name="hasta" id="hasta" required></td>
-                        <td>
-                        <select name="select" id="producto">
-                            <option value="p18">P18</option>
-                            <option value="sulfato">Sulfato Alumina</option>         
-                            <option value="ferrico">Férrico</option>         
-                            <option value="hb10">HB10</option>         
-                            <option value="sulfacid">SulfaCID</option>         
-                        </select>
-                        </td>
-                    </tr>
-                </tbody> 
-            </table>
-            <!--<b>Fecha inicial:</b><input type="date" name="desde" min="2022-01-01" id="desde" required><br><br>
+            <b>Fecha inicial:</b><input type="date" name="desde" min="2022-01-01" id="desde" required><br>
             <b>Fecha final  :</b><input type="date" name="hasta" id="hasta" required><br>
             <p></p>
             <label>Producto:</label>
@@ -47,19 +23,11 @@
                 <option value="hb10">HB10</option>         
                 <option value="sulfacid">SulfaCID</option>         
             </select>
-            <p></p>-->
+            <p></p>
             <input type ="button" name="Regresar" class="boton" value="Regresar" form = "Buscador" onclick="history.back()">            
             <input name="submit" class="boton" type="submit" value="Buscar" onclick="validarFechas()">            
         </form>  
     </fieldset>
-
-    <div id="resultado">MOSTRAR DATOS...
-
-        <input type ="text" value ="<?php echo $desde ?>">
-        <input type ="text" value ="<?php echo $hasta ?>">
-        <input type ="text" value ="<?php echo $producto ?>">
-
-    </div>
 
     <!--Validar que desde es menor que hasta -->
     <script>
