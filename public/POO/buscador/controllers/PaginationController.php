e4a9882f78188e04e11c5cae04fa88e235150d85
sergiocg <sergioc_g@hotmail.es>
2024-07-26 11:22:17 +0200

    Se agrega cierre de conexión de la base de datos y se arregla problema con el límite all

--- public/POO/buscador/controllers/PaginationController.php
+++ public/POO/buscador/controllers/PaginationController.php
@@ -12,35 +12,40 @@ class PaginationController {
             $limit = $_POST['limit'];            
             $page = (isset($_GET['page'])) ? $_GET['page'] : 1;
 
-            if ($limit == "all"){
+            if ($limit == "all"){                 
                 $offset = "";
+                $total_paginas = 1; //nuevo
             }
             else{
                 $offset = ($page - 1) * $limit;
+                $total_paginas = ceil($total_registros / $limit); //nuevo
             }
             
             $data = $model->getData($table, $startDate, $endDate, $limit, $page, $offset);
             $total_registros = $model->countRegistros($table, $startDate, $endDate);
 
-            if ($limit == "all"){
+            /*if ($limit == "all"){                
                 $total_paginas = 1;
             }
             else{
                 $total_paginas = ceil($total_registros / $limit);
-            }
+            }*/
 //PARTE NUEVA PARR EVITAR EL ERROR AL CAMBIAR LA BÚSQUEDA
-            if ($page > $total_paginas){                   
+            
+            if ($page > $total_paginas){                
                 $page = 1;
                 $offset = ($page - 1) * $limit;                
                 $data = $model->getData($table, $startDate, $endDate, $limit, $page, $offset);
             }
 //FIN PARTE NUEVA PARA EVITAR EL ERROR AL CAMBIAR LA BÚSQUEDA
 
-        } else { 
-            $data = [];            
-        }
-
+            } else {                 
+                $data = [];            
+            }
+        
         include 'views/paginationView.php';
+        $model->closeConecction();
+        
     }
 }
 ?>
