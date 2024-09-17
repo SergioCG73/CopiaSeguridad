9e25cf385fd9cddc0c47cece0907fa4e69364173
sergiocg <sergioc_g@hotmail.es>
2024-09-17 10:12:25 +0200

    Se eliminan comentarios

--- public/POO/p18/frmagregar.php
+++ public/POO/p18/frmagregar.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("../miconexion.php");
+    require_once("miconexion.php");
     $sql = $conexion->query("SELECT NumeroFabricacion FROM p18 ORDER BY NumeroFabricacion DESC LIMIT 1");
 
     $lastProduction = $sql->fetch();
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
     <link rel="icon" type="image/png" href="images/favicon.png"/>
     <title>Agregar P18</title>
 </head>
@@ -49,75 +49,6 @@
         </div>
     </div>
 
-    <script src="../js/script_agregar.js"></script>    
+    <script src="js/script_agregar.js"></script>    
 </body>
-<!--<script>
-    /*document.addEventListener("DOMContentLoaded", () =>{
-        const Reactor = document.getElementById("Reactor");
-        const Fabricacion = document.getElementById("fabricacion").value;
-        const FechaInicio = document.getElementById("FechaInicio");
-        const FechaFinal = document.getElementById("FechaFinal"); 
-        const PesoInicial = document.getElementById("PesoInicial");
-        const PesoFinal = document.getElementById("PesoFinal");
-        const Notas = document.getElementById("Notas");
-        const Boton = document.getElementById("boton");        
-        const Regresar = document.getElementById("regresar");
-
-        Regresar.addEventListener("click", () => {
-            window.location.href = "../indexp18.php";
-        })
-
-        Boton.addEventListener("click", () => {
-            if (!FechaInicio.value) {
-                    alert("Por favor, complete una fecha inicial.");
-                    return;
-            }
-
-            if (!FechaFinal.value) {
-                    FechaFinal.value = "2024-12-31T23:59"        
-                    globalThis.fecha2 = FechaFinal.value;
-            }
-
-            if (!PesoInicial.value) {
-                    alert("Por favor, complete un peso inicial.");
-                    return;
-            }
-
-            if (!PesoFinal.value) {
-                PesoFinal.value = null;
-            }
-
-            if (!Notas.value) {
-                    Notas.value = null;                    
-            }
-
-            campos = new FormData();
-            campos.append("FechaInicio", FechaInicio.value);
-            campos.append("Reactor", Reactor.value);
-            campos.append("NumFabricacion", Fabricacion);
-            campos.append("FechaFinal", FechaFinal.value);
-            campos.append("PesoInicial", PesoInicial.value);
-            campos.append("PesoFinal", PesoFinal.value);
-            campos.append("Notas", Notas.value);
-
-            try{
-                fetch("registrar.php", {
-                    method: "POST",
-                    body: campos                               
-            })
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .then(data => {
-                console.log(data); // Mostrar la respuesta en la consola
-                alert("Fabricación añadida correctamente"); // Mostrar un mensaje de éxito
-                window.location.href = "indexp18.php"; // Redirigir a otra página
-            })
-            
-            } catch (error) {
-                console.log(error);
-            }
-        });
-    });*/
-
-</script>-->
 </html>
