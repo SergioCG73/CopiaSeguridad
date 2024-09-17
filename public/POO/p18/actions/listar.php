d0107d72eb3d15e66256acf43da5339a9232e507
sergiocg <sergioc_g@hotmail.es>
2024-09-17 16:20:53 +0200

    Se crea el archivo en actions

---
+++ public/POO/p18/actions/listar.php
@@ -0,0 +1,43 @@
+<?php     
+    // Definir una función para manejar errores
+    function miErrorHandler($errno, $errstr, $errfile, $errline) {    
+        if ($errno === E_WARNING && strpos($errstr, 'require_once') !== false) { // Verifica si el error es del tipo E_WARNING        
+            echo "<script>alert('Error: El archivo requerido no se ha encontrado.');</script>"; // Mostrar una alerta o mensaje personalizado
+        }    
+        return false; // Retornar false para permitir que el manejador de errores predeterminado siga funcionando
+    }
+
+    set_error_handler("miErrorHandler"); // Establecer el manejador de errores personalizado
+    require_once("../miconexion.php"); // Intentar incluir el archivo
+    restore_error_handler(); // Restaurar el manejador de errores predeterminado después de la operación    
+    
+    $registros = isset($_POST['registros']) ? $_POST['registros'] : 10;    
+    $search_criteria = isset($_POST['search_criteria']) ? $_POST['search_criteria'] : null;      
+
+    $producciones = []; //Creamos el array producciones
+    $errors =['data'=> false];
+    
+    if ($search_criteria !="") {          
+        $registros = "80";        
+        $sql = $conexion->prepare("SELECT * FROM p18 
+                                    WHERE NumeroFabricacion 
+                                    LIKE '%" . $search_criteria . "%'
+                                    OR Reactor LIKE '%" . $search_criteria . "%'
+                                    OR Hora_Inicio LIKE '%" . $search_criteria . "%'
+                                    ORDER BY NumeroFabricacion DESC 
+                                    LIMIT " . $registros);
+    }
+    else {        
+        $sql = $conexion->prepare("SELECT * FROM p18 ORDER BY NumeroFabricacion DESC LIMIT $registros");
+    }    
+
+    $getProducciones = $sql->execute();  //Ejecuta el sql      
+    $getProducciones = $sql->fetchAll(PDO::FETCH_ASSOC);   //Crea un array asociativo con los datos obtenidos        
+        
+    if (count($getProducciones) > 0) {                
+        echo json_encode($getProducciones);        
+    }
+    else {        
+        echo json_encode($errors);        
+    }
+?>
