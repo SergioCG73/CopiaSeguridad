b2f8969034cef521d0c65f0ba366cab0823a1008
sergiocg <sergioc_g@hotmail.es>
2024-09-17 11:09:11 +0200

    Se añade manejador de errores al conectar

--- public/POO/p18/actions/registrar.php
+++ public/POO/p18/actions/registrar.php
@@ -1,5 +1,18 @@
-<?php 
-   require_once("miconexion.php");
+<?php
+
+    function miErrorHandler($errno, $errstr, $errfile, $errline) { // Definir una función para manejar errores
+        if ($errno === E_WARNING && strpos($errstr, 'require_once') !== false) { // Verifica si el error es del tipo E_WARNING        
+            echo "<script>alert('Error: El archivo requerido no se ha encontrado.');</script>"; // Mostrar una alerta o mensaje personalizado
+        }
+        
+        return false; // Retornar false para permitir que el manejador de errores predeterminado siga funcionando
+    }
+
+    set_error_handler("miErrorHandler"); // Establecer el manejador de errores personalizado
+
+    require_once("../miconexion.php"); // Intentar incluir el archivo
+
+    restore_error_handler(); // Restaurar el manejador de errores predeterminado después de la operación
 
     $FechaInicio = $_POST['FechaInicio'];     
     $Reactor = $_POST['Reactor'];
