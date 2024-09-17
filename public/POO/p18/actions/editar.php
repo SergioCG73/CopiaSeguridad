cb0029f33feb7047ac988227a70f7bcfee4fd973
sergiocg <sergioc_g@hotmail.es>
2024-09-17 16:20:42 +0200

    Modificaciones de estilo

--- public/POO/p18/actions/editar.php
+++ public/POO/p18/actions/editar.php
@@ -2,25 +2,16 @@
 echo "editar.php";
 
 // Definir una función para manejar errores
-function miErrorHandler($errno, $errstr, $errfile, $errline) {
-    // Verifica si el error es del tipo E_WARNING
-    if ($errno === E_WARNING && strpos($errstr, 'require_once') !== false) {
-        // Mostrar una alerta o mensaje personalizado
-        echo "<script>alert('Error: El archivo requerido no se ha encontrado.');</script>";
-    }
-
-    // Retornar false para permitir que el manejador de errores predeterminado siga funcionando
-    return false;
+function miErrorHandler($errno, $errstr, $errfile, $errline) {    
+    if ($errno === E_WARNING && strpos($errstr, 'require_once') !== false) { // Verifica si el error es del tipo E_WARNING        
+        echo "<script>alert('Error: El archivo requerido no se ha encontrado.');</script>"; // Mostrar una alerta o mensaje personalizado
+    }    
+    return false; // Retornar false para permitir que el manejador de errores predeterminado siga funcionando
 }
 
-// Establecer el manejador de errores personalizado
-set_error_handler("miErrorHandler");
-
-// Intentar incluir el archivo
-require_once("../miconexion.php");
-
-// Restaurar el manejador de errores predeterminado después de la operación
-restore_error_handler();
+set_error_handler("miErrorHandler"); // Establecer el manejador de errores personalizado
+require_once("../miconexion.php"); // Intentar incluir el archivo
+restore_error_handler(); // Restaurar el manejador de errores predeterminado después de la operación
 
 ?>
 
