2b6f039d6513fd375e2d5e693c0b20ee99b52e94
sergiocg <sergioc_g@hotmail.es>
2024-09-22 12:01:15 +0200

    Agregados a carpeta Eliminar

--- public/POO/p18/Eliminar/D_agregar.php
+++ public/POO/p18/Eliminar/D_agregar.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("miconexion.php");
+    require_once("../miconexion.php");
     $sql = $conexion->query("SELECT NumeroFabricacion FROM p18 ORDER BY NumeroFabricacion DESC LIMIT 1");
 
     $lastProduction = $sql->fetch();
@@ -12,55 +12,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/png" href="images/favicon.png"/>
     <title>Agregar P18</title>
 </head>
 <body>
-
-    <header class="encabezado"><h1>Agregar producción P18<h1></header>
-   
-    <div class="datos">
-        <fieldset><legend>DATOS FABRICACIÓN <?php echo $NextProduction ?></legend>
-            <label>Fabricación nº: </label>            
-            <input type="text" value="<?php echo $NextProduction; ?>" id="fabricacion" readonly>
-            <br>
-            <label>Fecha/Hora Inicio: </label>
-            <input type="datetime-local" name="FechaInicio" id="FechaInicio" required>   
-            <br>
-            <label>Fecha/Hora Final: </label>
-            <input type="datetime-local" name="FechaFinal" id="FechaFinal">
-            <br>
-            <label>Reactor: </label>    
+<header>Agregar fabricación P18</header>  
+    <div class="contenedor">
+        <div class="izquierda">
+            <fieldset class="agregar"><legend>DATOS FABRICACIÓN</legend> 
+            <p><label class="_100px">Fabricación nº: </label><input type="text" value="<?php echo $NextProduction; ?>" id="fabricacion" readonly class="gris"></p>
+            <label class="_100px">Reactor: </label>
             <select id="Reactor">
                     <option value="R200">R200</option>
                     <option value="R201">R201</option>
             </select>
-            <br>
-            <label>Peso Inicial: </label>
-            <input type="text" name="PesoInicial" id="PesoInicial" required>
-            <br> 
-            <label>Peso Final: </label>
-            <input type="text" name="PesoFinal" id="PesoFinal">
-            <br> 
-            <label>Nº Fab: </label>    
-        </fieldset>
+            <p><label class="_100px">Fecha/Hora Inicio: </label><input type="datetime-local" name="FechaInicio" id="FechaInicio" required></p>
+            <p><label class="_100px">Peso Inicial: </label><input type="text" name="PesoInicial" id="PesoInicial" required></p>
+            <p><label class="_100px">Fecha/Hora Final: </label><input type="datetime-local" name="FechaFinal" id="FechaFinal"></p>
+            <p><label class="_100px">Peso Final: </label><input type="text" name="PesoFinal" id="PesoFinal"></p>
+            </fieldset>
+        </div>
+        <div class="derecha">
+            <fieldset class="agregar"><legend>NOTAS</legend>
+                <textarea name="Notas" id="Notas"></textarea>
+            </fieldset>
+        </div>
+    </div>
+    
+    <div class="botonera_agregar">
+        <div class="izquierda">
+            <input type="button" value="Atrás" id="regresar" class="boton_agregar">
+        </div>
+        <div class="derecha">
+            <input type="button" value="Agregar" id="boton" class="boton_agregar">
+        </div>
     </div>
 
-    <div class="notas">
-        <fieldset><legend>NOTAS</legend>
-            <textarea name="Notas" id="Notas"></textarea>        
-        </fieldset>
-    </div>
-
-    
-    <div class="botonera">    
-        <input type="button" value="Regresar" id="regresar">
-        <input type="button" value="Enviar" id="boton">
-    </div>    
-    
+    <script src="../js/script_agregar.js"></script>    
 </body>
-<script>
-    document.addEventListener("DOMContentLoaded", () =>{
+<!--<script>
+    /*document.addEventListener("DOMContentLoaded", () =>{
         const Reactor = document.getElementById("Reactor");
         const Fabricacion = document.getElementById("fabricacion").value;
         const FechaInicio = document.getElementById("FechaInicio");
@@ -72,7 +64,7 @@
         const Regresar = document.getElementById("regresar");
 
         Regresar.addEventListener("click", () => {
-            window.location.href = "indexp18.php";
+            window.location.href = "../indexp18.php";
         })
 
         Boton.addEventListener("click", () => {
@@ -125,7 +117,7 @@
                 console.log(error);
             }
         });
-    });
+    });*/
 
-</script>
+</script>-->
 </html>
---
+++ public/POO/p18/Eliminar/D_script.js
@@ -0,0 +1,187 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    const search = document.getElementById("busqueda");        
+    const select = document.querySelector("select");
+    const resultados = document.getElementById("tabla_de_resultados");        
+    const tbody = document.getElementById("tbody");
+    const errorsContainer = document.getElementById("errorsContainer");    
+    const paginacion = document.getElementById("Paginacion");
+    const btnInicio = document.getElementById("btnInicio");
+    const btnNueva = document.getElementById("btnNueva");    
+    let search_criteria = null;
+    tbody.innerHTML = "";    
+    var registros = select.options[select.selectedIndex].value;    
+
+    console.log(search);
+
+    btnInicio.addEventListener("click", () => {
+        window.location.href = "../../portada.html";
+    });
+
+    btnNueva.addEventListener("click", () => {
+        window.location.href = "agregar.php";
+    });
+
+    select.addEventListener("change", (event) => {
+        registros = select.options[select.selectedIndex].value;
+        //console.log(value);
+        ListarProductos(registros, search_criteria);
+    });
+    
+    if (search_criteria === null) {
+        ListarProductos(registros, search_criteria);
+    }
+    
+    function ListarProductos(registros, search_criteria) {
+        const campos = new FormData();
+        campos.append("registros", registros);
+        campos.append("search_criteria", search_criteria);
+        tbody.innerHTML = "";
+        errorsContainer.innerHTML = "";
+
+        try {
+            fetch("listar.php", {
+                method: "POST",
+                body: campos
+            })
+            .then(response => response.json())
+            .then(response => {
+                if (response.length > 0) {                            
+                    FormatearDatos(response);                            
+                }
+                else {
+                    errorsContainer.innerHTML = "<p class='bold'>Este error no debería aparecer</p>";
+                }
+            });
+
+        } catch (error) {
+            console.error(error);
+        }
+}
+
+    if (search != null) {
+        search.addEventListener("input", (event) => {
+            search_criteria = event.target.value;
+            console.log(search_criteria);
+            const campos = new FormData();
+            campos.append("registros", registros);
+            campos.append("search_criteria", search_criteria);
+            tbody.innerHTML = "";
+            errorsContainer.innerHTML = "";
+
+            try{
+                fetch("listar.php", {
+                    method: "POST",
+                    body: campos
+                })
+                .then(response => response.json())
+                .then(response => {               
+                        //console.table(response);
+                        if (response.length > 0) {                            
+                            FormatearDatos(response);                            
+                        }
+                        else {
+                            errorsContainer.innerHTML = "<p class='bold'>No se encontraron datos para ese criterio de búsqueda</p>";
+                        }                                               
+                });
+            }
+            catch (error) {
+                console.log(error);                
+            }
+        });
+    }   
+
+    function FormatearDatos(response){
+        response.forEach (elemento => {
+                // Formatear Numero Fabricacion
+                const NumeroFabricacion = Number(elemento.NumeroFabricacion).toLocaleString('es-CL');
+
+                //Formatear Reactor
+                let Reactor = elemento.Reactor;
+                if (Reactor === "R200") {
+                    elemento.Reactor = `<span class="R200">${Reactor}</span>`;                    
+                } else if (Reactor === "R201") {
+                    elemento.Reactor = `<span class="R201">${Reactor}</span>`;
+                } else if (Reactor === "R202") {
+                    elemento.Reactor = `<span class="R202">${Reactor}</span>`;
+                }
+
+                // Formatear fecha y hora de inicio
+                const zeroPad = (val) => val.toString().padStart(2, "0"); 
+                let odate = new Date(elemento.Hora_Inicio);            
+                let year = odate.getFullYear();
+                let month = zeroPad(odate.getMonth()+1);
+                let day = zeroPad(odate.getDate());
+                let hour = zeroPad(odate.getHours());
+                let mins = zeroPad(odate.getMinutes());                
+                const horaInicio = day+'/'+month+'/'+year+' - '+hour+':'+mins;                
+
+                // Formatear fecha y hora de finalizacion
+                const zeroPadFinal = (val) => val.toString().padStart(2, "0"); 
+                let dateFinal = new Date(elemento.Hora_Finalizacion);
+                let yearFinal = dateFinal.getFullYear();
+                let monthFinal = zeroPadFinal(dateFinal.getMonth()+1);
+                let dayFinal = zeroPadFinal(dateFinal.getDate());
+                let hourFinal = zeroPadFinal(dateFinal.getHours());
+                let minsFinal = zeroPadFinal(dateFinal.getMinutes());                
+                const horaFinalizacion = dayFinal+'/'+monthFinal+'/'+yearFinal+' - '+hourFinal+':'+minsFinal;                
+                
+                // Formatear pesos                
+                const pesoInicial = Number(elemento.Peso_Inicial).toLocaleString("es-CL") + " Kg";
+                const pesoFinal = Number(elemento.Peso_Final).toLocaleString("es-CL") + " Kg";
+                
+                // Formatear la duración en horas y minutos                
+                    var horas = Math.floor(elemento.Duracion / 3600);1
+                    var minutos = ((elemento.Duracion - (horas * 3600)))/60;                
+                    let duracion = "";
+
+                    if (horas < 0 ) {
+                        duracion = "Falta Fecha Final";
+                    }
+                    else {
+                        duracion = `${horas}h y ${minutos} min`;
+                    }                    
+                
+                // Formatear el tiempo parado en minutos                
+                
+                var horas = Math.floor(elemento.Tiempo_Parado / 3600);
+                var minutos = ((elemento.Tiempo_Parado - (horas * 3600)))/60;                
+
+                if (horas < 1) {
+                    tiempoParado = `${minutos} min`;
+                }
+
+                if (horas > 1 && minutos > 1) {
+                    tiempoParado = `${horas}h y ${minutos} min`;
+                }                
+
+                //Formatear Notas
+
+                if (elemento.Notas != ""){
+                    elemento.Notas = "abc";
+                }
+                else {
+                    elemento.Notas = "";
+                }                
+                
+                tbody.innerHTML += `
+                    <tr>
+                        <!--<td>${elemento.NumeroFabricacion}</td>-->
+                        <td>${NumeroFabricacion}</td>
+                        <td>${elemento.Semana}</td>
+                        <td>${elemento.Reactor}</td>
+                        <!--<td>${Reactor}</td>-->
+                        <td>${horaInicio}</td>
+                        <td>${pesoInicial}</td>
+                        <td>${horaFinalizacion}</td>
+                        <td>${pesoFinal}</td>
+                        <td>${duracion}</td>
+                        <td>${tiempoParado}</td>
+                        <td>${elemento.Notas}</td>
+                        <td>
+                            <a href="alerta.php?id=${NumeroFabricacion}&estado=borrar"><image src="images/basura.png"></a>
+                            <a href="editar.php?id=${NumeroFabricacion}&estado=editar"><image src="images/editar.png"></a>
+                        </td>                        
+                    </tr>`;
+        })
+    }
+});
