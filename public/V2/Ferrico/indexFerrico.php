e7cff1023a0f6cd2f7383db40475dd7cb56f1809
sergiocg <sergioc_g@hotmail.es>
2024-10-27 10:49:30 +0100

    Ficheros de V2 Férrico

---
+++ public/V2/Ferrico/indexFerrico.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Férrico con PHP, MYSQL y AJAX">
+    <meta name="author" content="Sergio Cano González">
+    <link rel="icon" type="image/png" href="../Images/favicon.png">    
+    <link href="css/style.css" rel="stylesheet" type="text/css">    
+    <title>Férrico</title>
+</head>
+<body>    
+    <header>
+        <div class="main-container">
+            <div class="container">
+                <a href="#" class="logo">Cloruro Férrico</a>
+            </div>
+        </div>
+    </header>
+    <div class="left-container">
+        <div class="selector">
+            <label>Mostrar: </label>
+            <select id ="mostrar">
+                    <option value="2">2</option>
+                    <option value="4">4</option>
+                    <option value="6">6</option>
+                    <option value="8">8</option>
+                    <option value="10">10</option>
+                    <option value="12">12</option>
+            </select>
+        </div>
+
+        <div class="buscador">
+            <label for="busqueda">Buscar: </label>
+            <input type="search" id="busqueda" name="busqueda">
+        </div>
+    </div>
+
+    <div class="right-container">                    
+        <input id="btnInicio" type="button" value="Inicio" class="btnInicio">
+        <input id="btnNueva" type="button" value="Nueva Fabricación" class="btnNueva">
+    </div>
+
+    <section id="tabla_de_resultados">
+        <table id="tabla">
+            <thead>
+                <th>Fab Nº</th>
+                <th>Semana</th>                
+                <th>Fecha</th>
+                <th>Volumen inicial</th>
+                <th>Volumen final</th>
+                <th>Densidad</th>
+                <th>Riqueza</th>
+                <th>Ácido libre</th>
+                <th>Notas</th>
+                <th>Acciones</th>
+            </thead>
+            <tbody id="tbody">
+
+            </tbody>
+        </table>
+    </section>
+
+    <section id="errorsContainer" class="errorsContainer"></section>
+    <section id="Paginacion"></section>
+</body>
+
+<script src="js/script_indexFerrico.js"></script>
+
+</html>
---
+++ public/V2/Ferrico/js/script_indexFerrico.js
@@ -0,0 +1,147 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const search = document.getElementById("busqueda");  
+    const select = document.querySelector("select");
+    const resultados = document.getElementById("tabla_de_resultados");        
+    const tbody = document.getElementById("tbody");
+    const errorsContainer = document.getElementById("errorsContainer");    
+    const paginacion = document.getElementById("Paginacion");
+    const btnInicio = document.getElementById("btnInicio");
+    const btnNueva = document.getElementById("btnNueva");
+    let search_criteria = "";    
+    globalThis.Notas = "";
+    tbody.innerHTML = "";    
+    let registros = select.options[select.selectedIndex].value;    
+
+    btnInicio.addEventListener("click", () => {
+        window.location.href = "../../portada.html";
+    });    
+
+    btnNueva.addEventListener("click", () => {
+        fetch("frmagregar.php", {        
+             method: "HEAD" })  // Use HEAD to only fetch headers
+            .then(response => {
+                if (response.ok) {
+                    // Si el fichero existe, redirecciona a la página                    
+                    window.location.href = "frmagregar.php";
+                } else {
+                    // Si el fichero no existe, muestra una alerta
+                    alert("Error: El archivo 'frmagregar.php' no se encontró.");
+                }
+            })
+            .catch(error => {
+                console.error("Fetch error: ", error);
+                alert("Error: No se pudo verificar la existencia del archivo.");
+            });    });
+    
+
+    select.addEventListener("change", () => {
+        registros = select.options[select.selectedIndex].value;        
+        ListarProductos(registros, search_criteria);
+    });
+
+    // LLamamos a ListarProductos al cargar la página
+    ListarProductos(registros, search_criteria);
+
+    // Añade el evento escuchador para el campo de búsqueda, si este aparece
+    if (search) {
+        search.addEventListener("input", (event) => {
+            search_criteria = event.target.value;
+            console.log(search_criteria);
+            ListarProductos(registros, search_criteria);
+        });
+    }
+
+    function ListarProductos(registros, search_criteria) {
+        const campos = new FormData();
+        campos.append("registros", registros);
+        campos.append("search_criteria", search_criteria || ""); 
+    
+        tbody.innerHTML = "";
+        errorsContainer.innerHTML = "";
+    
+        console.log("search_criteria: ", search_criteria);
+        console.log("Registros: ", registros);
+    
+        fetch("actions/listar.php", {
+            method: "POST",
+            body: campos
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Response: ", data);
+            if (data.length > 0) {
+                FormatearDatos(data);
+            } else {
+                errorsContainer.innerHTML = "<p class='bold'>No se encontraron resultados.</p>";
+            }
+        })
+        .catch(error => {
+            console.error("Fetch error: ", error);
+            errorsContainer.innerHTML = "<p class='bold'>Error al traer los datos.</p>";
+        });
+    }
+
+    function FormatearDatos(response) {
+        response.forEach(elemento => {
+            // Formatear el número de fabricación
+            const NumeroFabricacionFormateada = Number(elemento.NumeroFabricacion).toLocaleString('es-CL');
+
+            // Formatear Fecha        
+            const zeroPad = (val) => val.toString().padStart(2, "0");
+            let odate = new Date(elemento.Fecha);
+            const Fecha = `${zeroPad(odate.getDate())}/${zeroPad(odate.getMonth() + 1)}/${odate.getFullYear()}`;
+            
+          // Formatear VoLÚMENES
+            const Volumen_Inicial = Number(elemento.Volumen_Inicial).toLocaleString("es-CL") + " Kg";
+            const Volumen_Final = Number(elemento.Volumen_Final).toLocaleString("es-CL") + " Kg";
+
+          // Formatear DENSIDAD
+            let DensidadFormateada = Number(elemento.Densidad);
+            DensidadFormateada = DensidadFormateada.toLocaleString('es-ES', {minimumFractionDigits: 2, maximumFractionDigits: 3});
+
+          // Formatear RIQUEZA
+          let RiquezaFormateada = Number(elemento.Riqueza);
+          RiquezaFormateada = RiquezaFormateada.toLocaleString('es-ES', {minimumFractionDigits: 2, maximumFractionDigits: 2});  
+          
+          // Formatear ÁCIDO LIBRE
+          let AcidoLibreFormateado = Number(elemento.Acido);
+          AcidoLibreFormateado = AcidoLibreFormateado.toLocaleString('es-ES', {minimumFractionDigits: 2, maximumFractionDigits: 2});  
+
+          // Formatear Notas
+          if (elemento.Notas != ""){ 
+                Notas = elemento.Notas;         
+                elemento.Notas = "abc";                
+            }
+          else {
+                elemento.Notas = "";
+            }
+            
+            // Genera el tbody de la tabla
+            tbody.innerHTML += `
+                                <tr>
+                                    <td>${NumeroFabricacionFormateada}</td> 
+                                    <td>${elemento.Semana}</td>
+                                    <td>${Fecha}</td>
+                                    <td>${Volumen_Inicial}</td>
+                                    <td>${Volumen_Final}</td>
+                                    <td>${DensidadFormateada}</td>
+                                    <td>${RiquezaFormateada}</td>
+                                    <td>${AcidoLibreFormateado}</td>
+                                    <td>${elemento.Notas}</td>
+                                    <td> <a href="actions/delete.php?id=${elemento.NumeroFabricacion}" onclick="return confirm('¿Estás seguro de que deseas borrar este registro?');">
+                                         <img src="../Images/basura_rojo_icon.png" alt="Borrar"></a>
+
+                                         <a href="frmeditar.php?id=${elemento.NumeroFabricacion}&                       
+                                         Fecha=${elemento.Fecha}&
+                                         Volumen_Inicial=${elemento.Volumen_Inicial}&
+                                         Volumen_Final=${elemento.Volumen_Final}&
+                                         Densidad=${elemento.Densidad}&
+                                         Riqueza=${elemento.Riqueza}&
+                                         AcidoLibre=${elemento.Acido}&
+                                         Notas=${Notas}">
+                                        <img src="../Images/editar_azul_icon.png" alt="Editar"></a>                       
+                                    </td> 
+                                </tr>`;  
+        });
+    }          
+});
