84f0b2ee3c08cf736f884982af52abd082484a12
sergiocg <sergioc_g@hotmail.es>
2024-11-18 12:07:00 +0100

    Creado para implementar delete al Sulfacid

---
+++ public/V2/Sulfacid/actions/delete.php
@@ -0,0 +1,41 @@
+<?php
+function miErrorHandler($errno, $errstr, $errfile, $errline) { // Definir una función para manejar errores
+    if ($errno === E_WARNING && strpos($errstr, 'require_once') !== false) { // Verifica si el error es del tipo E_WARNING        
+        echo "<script>alert('Error: El archivo requerido no se ha encontrado.');</script>"; // Mostrar una alerta o mensaje personalizado
+    }
+    
+    return false; // Retornar false para permitir que el manejador de errores predeterminado siga funcionando
+}
+
+set_error_handler("miErrorHandler"); // Establecer el manejador de errores personalizado
+require_once("../../../miconexion.php"); // Intentar incluir el archivo
+restore_error_handler(); // Restaurar el manejador de errores predeterminado después de la operación
+
+$id = $_GET['id'];
+$id = str_replace(".", "", $id); // Elimina el punto que nos llega por GET.
+
+if ($id > 0) {
+    // Prepara la instrucción SQL
+    $stmt = $conexion->prepare("DELETE FROM sulfacid WHERE NumeroFabricacion = $id");    
+
+    if ($stmt->execute()) {
+        // Si el borrado se realiza OK
+        echo "<script>
+            alert('Fabricación borrada');
+            window.location.href = '../indexSulfacid.php';
+        </script>";
+    } else {
+        // Si aparece un error al borrar
+        echo "<script>
+            alert('Hubo un error y la fabricación no se borró');
+            window.location.href = '../indexSulfacid.php';
+        </script>";
+    }
+    
+} else {
+    echo "<script>
+        alert('ID inválido');
+        window.location.href = '../indexSulfacid.php';
+    </script>";
+}
+?>
