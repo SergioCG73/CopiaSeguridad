ac210088e04b03ba5c9b3ab8858fe72acb8283ed
sergiocg <sergioc_g@hotmail.es>
2024-12-16 17:46:43 +0100

    Se modidica para usar con el SulfaCID

--- public/V2/Sulfacid/js/script_agregar.js
+++ public/V2/Sulfacid/js/script_agregar.js
@@ -1,27 +1,27 @@
-document.addEventListener("DOMContentLoaded", () =>{    
+document.addEventListener("DOMContentLoaded", () =>{
     const Fabricacion = document.getElementById("Fabricacion").value;
-    const Fecha = document.getElementById("Fecha");    
-    const Volumen = document.getElementById("Volumen");    
+    const Fecha = document.getElementById("Fecha");
+    const Volumen = document.getElementById("Volumen");
     const Densidad = document.getElementById("Densidad");
     const Riqueza = document.getElementById("Riqueza");
     const Ph = document.getElementById("Ph");
     const Notas = document.getElementById("Notas");
-    const BtnAgregar = document.getElementById("btnAgregar");   
+    const BtnAgregar = document.getElementById("btnAgregar");
     const BtnRegresar = document.getElementById("btnRegresar");
 
     BtnRegresar.addEventListener("click", () =>{
         window.location.href = "indexSulfacid.php";
     });
 
-    BtnAgregar.addEventListener("click", () => {       
+    BtnAgregar.addEventListener("click", () => {
         if (!Fecha.value) {
                 alert("Por favor, complete el campo fecha.");
                 return;
         }
 
         if (!Volumen.value) {
-                Volumen.value = "NULL";                
-        }        
+                Volumen.value = "NULL";
+        }
 
         if (!Densidad.value) {
             Densidad.value = null;
@@ -36,19 +36,19 @@ document.addEventListener("DOMContentLoaded", () =>{
         }
 
         if (!Notas.value) {
-                Notas.value = null;    //anterior Notas.value = "";    
+                Notas.value = null;    //anterior Notas.value = "";
         }
 
         campos = new FormData();
-        campos.append("Fecha", Fecha.value);                
-        campos.append("NumFabricacion", Fabricacion);   
-        campos.append("Volumen", Volumen.value);        
+        campos.append("Fecha", Fecha.value);
+        campos.append("NumFabricacion", Fabricacion)
+        campos.append("Volumen", Volumen.value);
         campos.append("Densidad", Densidad.value);
         campos.append("Riqueza", Riqueza.value);
-        campos.append("Ph", Ph.value);        
+        campos.append("Ph", Ph.value);
         campos.append("Notas", Notas.value);
 
-        fetch("actions/registrar.php", { 
+        fetch("actions/registrar.php", {
             method: "POST",
             body: campos
         })
@@ -57,10 +57,10 @@ document.addEventListener("DOMContentLoaded", () =>{
         .then(data => {
             //console.log("Data: ", data); // Mostrar la respuesta en la consola
             alert("Fabricación añadida correctamente"); // Mostrar un mensaje de éxito
-            window.location.href = "indexFerrico.php"; // Redirigir a otra página
+            window.location.href = "indexSulfacid.php"; // Redirigir a otra página
         })
         .catch(error => {
             console.log(error);
         })
-    });    
+    });
 });
